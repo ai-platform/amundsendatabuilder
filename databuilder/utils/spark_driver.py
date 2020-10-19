b65b05b2b36ab328b812c7298455ba65fd59ed56
Andrew Shi <munkyshi22@gmail.com>
2020-10-19 14:55:09 -0700

    Specify amundsen namespace with spark_driver (#11)

--- databuilder/utils/spark_driver.py
+++ databuilder/utils/spark_driver.py
@@ -26,15 +26,19 @@ def initSparkSession(minio_conf: MinioConf,
     conf.set('spark.hadoop.fs.s3a.fast.upload', str(True))
     conf.set('spark.hadoop.fs.s3a.path.style.access', str(True))
     conf.set('spark.hadoop.fs.s3a.impl', 'org.apache.hadoop.fs.s3a.S3AFileSystem')
+
     conf.set('spark.executor.instances', '1')
     conf.set('spark.executor.memory', '4g')
-    conf.set('spark.driver.memory', '2g')
-    conf.set('spark.driver.hostname', driver_ip)
+
     conf.set('spark.driver.host', driver_ip)
+    conf.set('spark.driver.hostname', driver_ip)
+    conf.set('spark.driver.memory', '2g')
+
+    conf.set('spark.kubernetes.authenticate.driver.serviceAccountName', 'spark')
+    conf.set('spark.kubernetes.container.image.pullPolicy', 'IfNotPresent')
     conf.set('spark.kubernetes.driver.container.image', spark_image)
     conf.set('spark.kubernetes.executor.container.image', spark_image)
-    conf.set('spark.kubernetes.container.image.pullPolicy', 'IfNotPresent')
-    conf.set('spark.kubernetes.authenticate.driver.serviceAccountName', 'spark')
+    conf.set('spark.kubernetes.namespace', 'amundsen')
 
     sc = pyspark.SparkContext(master=k8s_master, appName='amundsen-index-minio', conf=conf)
     return sc, SparkSession(sc)
